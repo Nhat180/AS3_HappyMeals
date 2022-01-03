c7a462664c23bde3d65ba3c993cb10abbefa4cce
tongnguyensinoda <lamnguyensinoda@gmail.com>
2022-01-03 11:22:58 +0700

    Update MapsActivity.java

--- app/src/main/java/com/example/as3_happymeals/MapsActivity.java
+++ app/src/main/java/com/example/as3_happymeals/MapsActivity.java
@@ -1,9 +1,55 @@
 package com.example.as3_happymeals;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.annotation.SuppressLint;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SearchView;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,12 +57,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.as3_happymeals.databinding.ActivityMapsBinding;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    private BottomNavigationView bottomNavigationView;
+    protected FusedLocationProviderClient client;
+    protected LocationRequest mLocationRequest;
+    private FloatingActionButton fab;
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 99;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +82,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+//        //https://www.youtube.com/watch?v=ywqCTCR2a0w
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        Toast.makeText(getApplicationContext(), "HOme page", Toast.LENGTH_SHORT ).show();
+                        break;
+
+                    case R.id.action_info:
+                        Toast.makeText(getApplicationContext(), "Info", Toast.LENGTH_SHORT ).show();
+                        break;
+
+                    case R.id.action_map:
+                        Toast.makeText(getApplicationContext(), "Map", Toast.LENGTH_SHORT ).show();
+                        break;
+                    case R.id.action_logout:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+                        builder.setTitle("Logout Confirmation!").
+                                setMessage("Do you want to logout?");
+                        // back to login screen
+                        builder.setPositiveButton("Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Intent i = new Intent(getApplicationContext(),
+                                                LoginActivity.class);
+                                        startActivity(i);
+                                        finish();
+//
+//                                            firebaseAuth.signOut();
+//                                            startActivity(new Intent(MapsActivity.this, WelcomeActivity.class));
+//                                            finish();
+
+                                    }
+                                });
+
+                        // Cancel
+                        builder.setNegativeButton("No",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        AlertDialog alert11 = builder.create();
+                        alert11.show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        fab = findViewById(R.id.my_position);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrentLocation();
+            }
+        });
+
+    }
+
+    // Get all sites on map
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableMyLocation();
+    }
+
+    // Get current location
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        client.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Location location = task.getResult();
+                gotoLocation(location.getLatitude(), location.getLongitude());
+            }
+        });
+    }
+
+    // Go to current location
+    private void gotoLocation(double latitude, double longitude) {
+        LatLng latLng = new LatLng(latitude, longitude);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        mMap.moveCamera(cameraUpdate);
     }
 
     /**
@@ -39,13 +181,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        requestPermission();
+        client = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
         mMap = googleMap;
-
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        startLocationUpdate();
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+    }
+
+    // Update the current location
+    @SuppressLint({"MissingPermission", "RestrictedApi"})
+    private void startLocationUpdate() {
+        mLocationRequest = new LocationRequest();
+        client.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                LatLng latLng = new LatLng(locationResult.getLastLocation().getLatitude(),
+                        locationResult.getLastLocation().getLongitude());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+            }
+        }, null);
+    }
+
 }
+
