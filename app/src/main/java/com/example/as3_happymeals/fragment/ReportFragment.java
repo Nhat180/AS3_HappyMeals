d9bd2580933f1fc1b15d9c284037e84f155ce8aa
Nhat180 <s3740975@rmit.edu.vn>
2022-01-14 01:12:17 +0700

    Update backend for report fragment

--- app/src/main/java/com/example/as3_happymeals/fragment/ReportFragment.java
+++ app/src/main/java/com/example/as3_happymeals/fragment/ReportFragment.java
@@ -15,38 +15,72 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.example.as3_happymeals.MapsActivity;
 import com.example.as3_happymeals.R;
+import com.example.as3_happymeals.model.Site;
+import com.example.as3_happymeals.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReportFragment extends Fragment {
-    private TextView textView;
+    private TextView siteName, leaderName, totalMember, totalComment;
     private ListView listView;
     private Button viewBtn, cancelButton;
-    private String name[] = {"Ta Quoc Thang", "Nguyen Van TOng", "Ho Duy Hoang", "Pham Thanh Dat", "Nguyen Minh Nhat", "Ngoc Hue", "Bill Hoang", "Trung Le", "Quang hai", "Hai Que", "Long Ngo", "Huy Ngo", "Man Lan", "Hoang Yen", "Johnsom", "Thong Nguyen"};
+    private List<String> members = new ArrayList<>();
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Site site = MapsActivity.site;
+    private User user = MapsActivity.user;
+    private String role = MapsActivity.role;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.report_fragment, container, false);
+
+        siteName = root.findViewById(R.id.report_sitename);
+        leaderName = root.findViewById(R.id.report_leadername);
+        totalMember = root.findViewById(R.id.report_totalpeople);
+        totalComment = root.findViewById(R.id.report_totalcomment);
         viewBtn = root.findViewById(R.id.viewMemBtn);
+
+        siteName.setText(site.getName());
+        leaderName.setText("Leader: " + site.getLeaderName());
+        totalMember.setText("Total of Registered People: " + site.getTotalPeople());
+        totalComment.setText("Total of Comments: " + site.getTotalComment());
+
+        if (role.equals("0")) {
+            viewBtn.setVisibility(View.GONE);
+        }
+
         viewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
                 AlertDialog builder = new AlertDialog.Builder(getActivity()).create();
                 View form = LayoutInflater.from(getActivity()).inflate(R.layout.list_member_dialog, null);
                 builder.setView(form);
-                listView = form.findViewById(R.id.list);
-                ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, name);
-                listView.setAdapter(arrayAdapter);
+
+                db.collection("sites").document(site.getLeaderUid()).get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                DocumentSnapshot documentSnapshot = task.getResult();
+                                members = (List<String>) documentSnapshot.get("userRegistered");
+                                listView = form.findViewById(R.id.list);
+                                ArrayAdapter arrayAdapter =
+                                        new ArrayAdapter(getActivity(),
+                                                android.R.layout.simple_list_item_1, members);
+                                listView.setAdapter(arrayAdapter);
+                            }
+                        });
+
                 cancelButton = form.findViewById(R.id.cancelPackageBtn);
 
-//                savePackageBtn.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//
-//                        Toast.makeText(getActivity(),"Package saved",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
+
                 cancelButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
